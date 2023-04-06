000f1a7acb319fe41c5c53e31bf59e1865fc416f
Vittorio <tonibusatta@gmail.com>
2023-04-06 22:30:06 +0400

    /

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ function Layout({ children }) {
     <>
       <Header />
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- pages/monitoring/index.js
+++ pages/monitoring/index.js
@@ -1,32 +1,65 @@
-import React from "react";
+import { gsap } from "gsap";
+import React, { useEffect, useRef, useState } from "react";
 
-const article = ({ array }) => {
+const article = ({ array, openPreview }) => {
+  const { subtitle, title, description } = array;
   return (
-    <article className="monitoring__article">
-      <div>
-        <span>{array.subtitle}</span>
-        <h2>{array.title}</h2>
-      </div>
-      <div className="monitoring__article__bottom">
-        <p>{array.description}</p>
-        <svg
-          width="44"
-          height="40"
-          viewBox="0 0 24 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13.9424 0H10.7994V8.76712H0.741699V11.2329H10.7994V20H13.9424V11.2329H24.0001V8.76712H13.9424V0Z"
-            fill="#0B632E"
-          />
-        </svg>
-      </div>
-    </article>
+    <>
+      <article className="monitoring__article" onClick={openPreview}>
+        <div>
+          <span>{subtitle}</span>
+          <h2>{title}</h2>
+        </div>
+        <div className="monitoring__article__bottom">
+          <p>{description}</p>
+          <svg
+            width="44"
+            height="40"
+            viewBox="0 0 24 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.9424 0H10.7994V8.76712H0.741699V11.2329H10.7994V20H13.9424V11.2329H24.0001V8.76712H13.9424V0Z"
+              fill="#0B632E"
+            />
+          </svg>
+        </div>
+      </article>
+    </>
   );
 };
 
 function Monitoring() {
+  const [previewIndex, setPreviewIndex] = useState(2);
+  const [previewActive, setPreviewActive] = useState(false);
+  const overlayInner = useRef([]);
+
+  useEffect(() => {
+    gsap.set(overlayInner.current, {
+      x: "-100%",
+    });
+  }, []);
+
+  const openPreview = (index) => {
+    console.log("index", index);
+    setPreviewIndex(index);
+    setPreviewActive(true);
+
+    gsap.to(overlayInner.current[index], {
+      x: 0,
+      duration: 0.6,
+    });
+  };
+
+  const closePreview = () => {
+    setPreviewActive(false);
+    gsap.to(overlayInner.current[previewIndex], {
+      x: "-100%",
+      duration: 0.6,
+    });
+  };
+
   const legalMonitoring = {
     subtitle: "Veille Juridique",
     title:
@@ -42,16 +75,52 @@ function Monitoring() {
       "Découvrez les métiers susceptibles d'être menacés par l'Intelligence Artificielle (IA) et l'automatisation croissante des tâches dans cet article. Il examine les secteurs d'activité où l'IA pourrait avoir un impact significatif sur l'emploi, tels que la production manufacturière, le commerce de détail, ou encore le secteur des services. L'article se penche également sur les métiers qui pourraient être transformés ou enrichis par l'utilisation de l'IA.",
   };
 
+  const array = [computerMonitoring, legalMonitoring];
+
+  console.log("previewIndex", previewIndex, "previewActive", previewActive);
+
   return (
     <section className="landing">
       <h1 className="landing__title">Veilles</h1>
       <div className="monitoring">
         <div className="monitoring__content">
-          {article({
-            array: computerMonitoring,
+          {array.map((array, index) => {
+            return article({
+              array,
+              openPreview: () => openPreview(index),
+            });
           })}
-          {article({
-            array: legalMonitoring,
+        </div>
+        {array.map((array, index) => {
+          return (
+            <div
+              key={index}
+              className={`monitoring__preview ${
+                previewActive && previewIndex === index
+                  ? "monitoring__preview--active"
+                  : ""
+              }`}
+            >
+              <h1>{array.title}</h1>
+              <h2
+                onClick={() => {
+                  closePreview();
+                }}
+              >
+                CLOSE
+              </h2>
+            </div>
+          );
+        })}
+        <div className="overlay">
+          {array.map((item, index) => {
+            return (
+              <div
+                key={item.id}
+                className="overlay__inner"
+                ref={(el) => (overlayInner.current[index] = el)}
+              ></div>
+            );
           })}
         </div>
       </div>
